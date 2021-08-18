f450fbc243b00f3a508f6c2d8e52d1c41df6a4a6
johnathanthedev <johnathanguzman17@gmail.com>
2021-08-18 17:59:16 -0500

    Output to console evening and reg wage total. Need overtime.

--- models/Person.rb
+++ models/Person.rb
@@ -1,10 +1,12 @@
 class Person
-    attr_accessor :full_name, :person_id, :shifts, :earnings
+    attr_accessor :full_name, :person_id, :shifts, :earnings, :evening_hours_worked, :non_evening_hours_worked
     
     def initialize(full_name, person_id)
         @full_name = full_name
         @person_id = person_id
         @shifts = []
+        @evening_hours_worked = 0
+        @non_evening_hours_worked = 0
         @earnings = 0
     end
 end
